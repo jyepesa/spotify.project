79fbe26f2491d79f6d28d972df8d12d323e548c9
jyepesa <juank_713@hotmail.com>
2025-02-27 18:38:10 +0100

    add search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+function SearchBar(props) {
+    const [ input, setInput ] = useState("")
+    const handleChange = (e) => {
+        setInput(e.target.value)  
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();   
+        props.onSearch(input)
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter Song/Artist" onChange={handleChange} value={input} />
+            <input type="submit" value="Search" /> 
+        </form>
+    )  
+}
+
+export default SearchBar
